f2bf2fe7b322d9256d9480cb1d62a31720c5eba8
okopel <okopel@gmail.com>
2018-12-24 23:42:59 -0800

    v4.1

--- Client.h
+++ Client.h
@@ -74,9 +74,12 @@ void ConnectClient(int portNumber, string ipPath) {
     while (true) {
         this_thread::sleep_for(0.1s);
         if (!msgToServer.empty()) {
+            clientLocker.lock();
             const char *c = msgToServer.c_str();
-            //n = write(sockfd, c, strlen(buffer));
+            clientLocker.unlock();
+
             n = send(sockfd, c, strlen(buffer), MSG_EOR);//todo MSG_OOB?
+
             clientLocker.lock();
             msgToServer = "";
             clientLocker.unlock();
